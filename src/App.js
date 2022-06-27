142678e2e7bd740dd0c4d3d46c9dbc9bc35b7797
Mac Kadek <mackadek@Macs-MacBook-Pro.local>
2022-06-27 21:29:08 +0700

    updates 27/06/22 21.29

--- src/App.js
+++ src/App.js
@@ -18,16 +18,19 @@ function DummyDb() {
   const users = [
     {
       id: 1,
+      photo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       name: "Mama Setiawan",
       review: "Harganya murah tapi kualitas bukan kaleng-kaleng nih",
     },
     {
       id: 2,
+      photo: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       name: "Mama dina",
       review: "Harganya murah Banget...",
     },
     {
       id: 3,
+      photo: "https://images.pexels.com/photos/2120114/pexels-photo-2120114.jpeg?auto=compress&cs=tinysrgb&w=400",
       name: "Mama adis",
       review: "Harganya murah Banget...",
     },
@@ -36,7 +39,7 @@ function DummyDb() {
   const listReviews = users.map((itemReview) => 
     <div className="item">
       <img
-        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        src={itemReview.photo}
         alt=""
       />
       <div className="user">
